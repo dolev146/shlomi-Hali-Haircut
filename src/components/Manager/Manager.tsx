6ea51e9ae45473cebf2957554e21ea3a152c4333
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2024-12-29 23:49:59 +0200

    shlomi page

--- src/components/Manager/Manager.tsx
+++ src/components/Manager/Manager.tsx
@@ -1,9 +1,51 @@
-import { Box } from "@mui/material";
+import { Box, Card, CardContent, Typography, Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getAllAppointments } from "./utils/getAllAppointments";
+import { Appointment } from "../AppointmentScheduler/types";
 
 const Manager = () => {
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      const data = await getAllAppointments();
+      setAppointments(data);
+    };
+    fetchAppointments();
+  }, []);
+
   return (
-    <Box color={"black"}>
-      כאן יהיו דברים שקשורים לניהול למשל לראות את כל התורים הקיימים והפנויים
+    <Box color={"black"} style={{ backgroundColor: "white" }} p={2}>
+      <Typography variant="h4" gutterBottom>
+        הפגישות שלך
+      </Typography>
+      <Grid container spacing={2}>
+        {appointments.map((appointment, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card>
+              <CardContent>
+                <div dir="rtl">
+                  <Typography variant="h6" gutterBottom>
+                    שם: {appointment.fullName}
+                  </Typography>
+                  <Typography variant="body1">
+                    שירות: {appointment.service.serviceName}
+                  </Typography>
+                  <Typography variant="body2">
+                    תאריך: {appointment.date}
+                  </Typography>
+                  <Typography variant="body2">
+                    שעה: {appointment.time}
+                  </Typography>
+                  <Typography variant="body2">
+                    טלפון: {appointment.phoneNumber}
+                  </Typography>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
