c74f37f2704208f7b3145c91e8ebd6841800405a
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2025-02-04 20:01:39 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,35 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    manifest: {
-      icons : [
-        {
-          src: "/icons/937.jpg",
-          sizes: "937x937",
-          type : "image/jpg",
-          purpose: "any maskable"
-        }
-      ]
-    }
-  })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: false,
+      },
+      manifest: {
+        name: "Shlomi Hali HairCut",
+        short_name: "Haircut",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff", // IMPORTANT to avoid the warning
+        icons: [
+          {
+            src: "/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: true, // Set to 'true' to make it accessible on the public network
     port: 3000, // Optional: specify the port (default is 5173)
